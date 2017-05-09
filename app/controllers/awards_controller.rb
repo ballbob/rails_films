7c2e250da4db2c128d2c52ee508a7a3ea1ed9d04
Colin Farquhar <cifarquhar@gmail.com>
2017-05-09 11:52:29 +0100

    format json output

--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -2,7 +2,17 @@ class AwardsController < ApplicationController
 
   def index 
     awards = Award.where({film: params[:film_id] })
-    render json: awards
+    render json: awards.as_json({
+      only: [:name],
+      include: {
+        film: {
+          only: [:title, :genre]
+        },
+        talent: {
+          only: [:name, :job]
+          }
+        }
+      })
   end
 
 end
